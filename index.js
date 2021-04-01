be2a1af7f8e8978747adbefb25594e2b0fdd4d85
Sadnan Kibria Kawshik <sadnan.24csedu.015@gmail.com>
2021-04-02 00:32:49 +0600

    Clickable 1st part

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function randomStringDisplay() {
     var str = ["A", "B", "C", "D", "E"];
-    var suggestions = document.getElementsById("tests");
+    var suggestions = document.getElementById("tests");
     suggestions.value = str.join("\n");
 }
+
+var input = document.querySelector('#rx');
+input.addEventListener('keyup', (e) => {
+    if (e.keyCode == 13) {
+        var x = input.value;
+        console.log(x);
+        randomStringDisplay();
+        e.preventDefault();
+    }
+});
