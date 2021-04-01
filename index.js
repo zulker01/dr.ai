9c508edfcb7f7e297c9c5413fc960f51ff31007e
zulker01 <zulker02@gmail.com>
2021-04-02 01:57:08 +0600

    updated values change with previous change in dx cc IX

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function randomStringDisplay() {
     //var suggestions = document.getElementById("tests");
    // suggestions.value = str.join("\n");
 }
-var str = ["Serum Ige", " CBC", " Chest CT ", "USG whole abdomen", "GeneXpert for TB"];
+var str = ["Serum Ige ", " CBC ", " Chest CT ", "USG whole abdomen ", "GeneXpert for TB ","MRI whole skull ","Endoscopy ","Urine culture ","ICT for TB "];
+var prevStr="lalalal"
 var input = document.querySelector('#rx');
 input.addEventListener('keyup', (e) => {
     if (e.keyCode == 13) {
@@ -15,68 +16,92 @@ input.addEventListener('keyup', (e) => {
         document.getElementById("suggest3").innerHTML = str[2];
         document.getElementById("suggest4").innerHTML = str[3];
         document.getElementById("suggest5").innerHTML = str[4];
-        document.getElementById("suggest6").innerHTML = str[0];
-        document.getElementById("suggest7").innerHTML = str[1];
-        document.getElementById("suggest8").innerHTML = str[2];
-        document.getElementById("suggest9").innerHTML = str[3];
+        document.getElementById("suggest6").innerHTML = str[5];
+        document.getElementById("suggest7").innerHTML = str[6];
+        document.getElementById("suggest8").innerHTML = str[7];
+        document.getElementById("suggest9").innerHTML = str[8];
         
         e.preventDefault();
     }
 });
 
 function fun1(){
-    document.getElementById("dx").innerHTML = str[0].concat(" is selected");
-    document.getElementById("cc").innerHTML = str[0].concat(" is selected");
-    document.getElementById("rx").innerHTML = str[0].concat(" is selected");
-    document.getElementById("ix").innerHTML = str[0].concat(" is selected");
+    var newStr = prevStr.concat(str[0]);
+    prevStr = newStr;
+    document.getElementById("dx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("cc").innerHTML = newStr.concat(" is selected");
+    document.getElementById("rx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("ix").innerHTML = newStr.concat(" is selected");
 }
 
 function fun2(){
-    document.getElementById("dx").innerHTML = str[1].concat(" is selected");
-    document.getElementById("cc").innerHTML = str[1].concat(" is selected");
-    document.getElementById("rx").innerHTML = str[1].concat(" is selected");
-    document.getElementById("ix").innerHTML = str[1].concat(" is selected");
+    var newStr = prevStr.concat(str[1]);
+    prevStr = newStr;
+    document.getElementById("dx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("cc").innerHTML = newStr.concat(" is selected");
+    document.getElementById("rx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("ix").innerHTML = newStr.concat(" is selected");
 }
 
 function fun3(){
-    document.getElementById("dx").innerHTML = str[2].concat(" is selected");
-    document.getElementById("cc").innerHTML = str[2].concat(" is selected");
-    document.getElementById("rx").innerHTML = str[2].concat(" is selected");
-    document.getElementById("ix").innerHTML = str[2].concat(" is selected");
+    var newStr = prevStr.concat(str[2]);
+    prevStr = newStr;
+    document.getElementById("dx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("cc").innerHTML = newStr.concat(" is selected");
+    document.getElementById("rx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("ix").innerHTML = newStr.concat(" is selected");
 }
 
 function fun4(){
-    document.getElementById("dx").innerHTML = str[3].concat(" is selected");
-    document.getElementById("cc").innerHTML = str[3].concat(" is selected");
-    document.getElementById("rx").innerHTML = str[3].concat(" is selected");
-    document.getElementById("ix").innerHTML = str[3].concat(" is selected");
+    var newStr = prevStr.concat(str[3]);
+    prevStr = newStr;
+    document.getElementById("dx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("cc").innerHTML = newStr.concat(" is selected");
+    document.getElementById("rx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("ix").innerHTML = newStr.concat(" is selected");
 }
 
 function fun5(){
-    document.getElementById("dx").innerHTML = str[4].concat(" is selected");
-    document.getElementById("cc").innerHTML = str[4].concat(" is selected");
-    document.getElementById("rx").innerHTML = str[4].concat(" is selected");
-    document.getElementById("ix").innerHTML = str[4].concat(" is selected");
+    var newStr = prevStr.concat(str[4]);
+    prevStr = newStr;
+    document.getElementById("dx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("cc").innerHTML = newStr.concat(" is selected");
+    document.getElementById("rx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("ix").innerHTML = newStr.concat(" is selected");
 }
 
-function fun5(){
-    document.getElementById("dx").innerHTML = str[0].concat(" is selected");
-    document.getElementById("cc").innerHTML = str[0].concat(" is selected");
-    document.getElementById("rx").innerHTML = str[0].concat(" is selected");
-    document.getElementById("ix").innerHTML = str[0].concat(" is selected");
-}
+
 
 function fun6(){
-    
-    document.getElementById("dx").innerHTML = str[1].concat(" is selected");
-    document.getElementById("cc").innerHTML = str[1].concat(" is selected");
-    document.getElementById("rx").innerHTML = str[1].concat(" is selected");
-    document.getElementById("ix").innerHTML = str[1].concat(" is selected");
+    var newStr = prevStr.concat(str[5]);
+    prevStr = newStr;
+    document.getElementById("dx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("cc").innerHTML = newStr.concat(" is selected");
+    document.getElementById("rx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("ix").innerHTML = newStr.concat(" is selected");
 }
 
 function fun7(){
-    document.getElementById("dx").innerHTML = str[2].concat(" is selected");
-    document.getElementById("cc").innerHTML = str[2].concat(" is selected");
-    document.getElementById("rx").innerHTML = str[2].concat(" is selected");
-    document.getElementById("ix").innerHTML = str[2].concat(" is selected");
+    var newStr = prevStr.concat(str[6]);
+    prevStr = newStr;
+    document.getElementById("dx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("cc").innerHTML = newStr.concat(" is selected");
+    document.getElementById("rx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("ix").innerHTML = newStr.concat(" is selected");
+}
+function fun8(){
+    var newStr = prevStr.concat(str[7]);
+    prevStr = newStr;
+    document.getElementById("dx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("cc").innerHTML = newStr.concat(" is selected");
+    document.getElementById("rx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("ix").innerHTML = newStr.concat(" is selected");
+}
+function fun9(){
+    var newStr = prevStr.concat(str[8]);
+    prevStr = newStr;
+    document.getElementById("dx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("cc").innerHTML = newStr.concat(" is selected");
+    document.getElementById("rx").innerHTML = newStr.concat(" is selected");
+    document.getElementById("ix").innerHTML = newStr.concat(" is selected");
 }
