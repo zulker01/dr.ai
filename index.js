c3277d8d32b39baaed5e47d7684f4e242e0e4d9f
Sadnan Kibria Kawshik <sadnan.24csedu.015@gmail.com>
2021-04-02 04:15:42 +0600

    javascript option portion done

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 function randomStringDisplay() {
-    var str = ["A", "B", "C", "D", "E"];
-    var suggestions = document.getElementById("tests");
-    suggestions.value = str.join("\n");
+    var str = ["Serum Ige ", " CBC ", " Chest CT ", "USG whole abdomen ", "GeneXpert for TB ", "MRI whole skull ", "Endoscopy ", "Urine culture ", "ICT for TB "];
+    var items = document.querySelectorAll("#tests li");
+    var tab = [];
+    var ind;
+
+    // add values to the array
+    for (var i = 0; i < items.length; i++) {
+
+        items[i].innerHTML = str[i];
+
+        tab.push(items[i].innerHTML);
+    }
+
+    // get selected element index
+    for (var i = 0; i < items.length; i++) {
+        items[i].onclick = function() {
+
+            ind = tab.indexOf(this.innerHTML);
+            console.log(this.innerHTML + " Index = " + ind);
+            var x = items[ind].innerHTML;
+            myfunction(x);
+
+        };
+    }
+
 }
+
+function myfunction(x) {
+    var newStr = x + " is selected";
+    document.querySelector('#dx').value += "\n" + newStr;
+    document.querySelector('#rx').value += "\n" + newStr;
+    document.querySelector('#cc').value += "\n" + newStr;
+    document.querySelector('#ix').value += "\n" + newStr;
+}
+
+
+
 
 var input = document.querySelector('#rx');
 input.addEventListener('keyup', (e) => {
